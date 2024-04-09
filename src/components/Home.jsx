fc861d2a0a8c01d06d58619994ff4d6cdd068182
Suvam <suvamgpt8@gmail.com>
2024-04-10 05:05:06 +0530

    changed home https

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () =>{
 
     return (
         <div>
-            <p>This is home after changing www</p>
+            <p>This is home after changing www https</p>
             {loading?<p>Loading data...</p> :
         <MatTable data={myData}/>
             }
